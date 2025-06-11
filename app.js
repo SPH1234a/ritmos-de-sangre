0aba4034d009f45ba4b261a5b8773052d153cc16
SPH1234a <steve.oga1111@gmail.com>
2025-06-11 00:04:10 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,5 @@
 
 function mostrarSeccion(id) {
-  const secciones = document.querySelectorAll('main section');
-  secciones.forEach(sec => sec.style.display = 'none');
-  const activa = document.getElementById(id);
-  if (activa) activa.style.display = 'block';
+  document.querySelectorAll('section').forEach(sec => sec.classList.remove('active'));
+  document.getElementById(id).classList.add('active');
 }
-
-function toggleSubmenu() {
-  const submenu = document.querySelector('.submenu');
-  if (submenu.style.display === 'block') {
-    submenu.style.display = 'none';
-  } else {
-    submenu.style.display = 'block';
-  }
-}
-
-function expandirImagen(img) {
-  const modal = document.getElementById('modal');
-  const imagenExpandida = document.getElementById('imagenExpandida');
-  imagenExpandida.src = img.src;
-  modal.style.display = 'flex';
-}
-
-function cerrarModal() {
-  document.getElementById('modal').style.display = 'none';
-}
-
-window.onload = () => {
-  mostrarSeccion('inicio');
-};
